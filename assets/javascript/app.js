9c687fc93eec7a4afcd5da87697cdf3212daec07
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-08 22:23:54 -0500

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBUzClX_NxCqGXDswwFfk7GuHG719hbxPo",
+    authDomain: "rpsgame-31571.firebaseapp.com",
+    databaseURL: "https://rpsgame-31571.firebaseio.com",
+    projectId: "rpsgame-31571",
+    storageBucket: "rpsgame-31571.appspot.com",
+    messagingSenderId: "983422850754"
+  };
+  firebase.initializeApp(config);
+
+  
