8b94def843b713a2e1ee114ed59c997813ea4fcf
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-12 10:21:36 -0500

    firebase connection established

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
+// set inital conditions of object to start game
+
+ var readyGame = {
+     playerOne : {
+        wins: 0,
+        loses: 0,
+        name = '',
+        letWinner = false,
+     },
+     playerTwo : {
+         wins: 0,
+         loses: 0,
+         name = '',
+         letWinner = false,
+     }
+ }
+
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyBUzClX_NxCqGXDswwFfk7GuHG719hbxPo",
@@ -8,5 +26,25 @@ var config = {
     messagingSenderId: "983422850754"
   };
   firebase.initializeApp(config);
+// create a reference to firebase
+  var database = firebase.database();
 
+  var connectionsRef = database.ref("/connections")
+
+  var connectedRef = database.ref(".info/connected");
   
+  if (snap.val()) {
+      var con = connectionsRef.push(true);
+      //remove user from connections when they disconnect
+      con.onDisconnect().remove();
+  }; 
+  $("#submitBtn").on("click", function(){
+
+  })
+  //updates page when playerOne info has changed
+  database.ref("/playerOne").on("value", function(snapshot){
+      if (snapshot.child("name").exists()) {
+        name = snapshot.val().name;
+      }
+  })
+
