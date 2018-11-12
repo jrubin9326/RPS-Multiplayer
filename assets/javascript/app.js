9b2f35b3a951c871b7c2ec87e56d4ecebe9bd36b
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-12 12:38:24 -0500

    player 1 info

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,15 +3,16 @@
  var readyGame = {
      playerOne : {
         wins: 0,
-        loses: 0,
-        name = '',
-        letWinner = false,
+        losses: 0,
+        name : "",
+        pick : "",
      },
      playerTwo : {
          wins: 0,
-         loses: 0,
-         name = '',
-         letWinner = false,
+         losses: 0,
+         name : "",
+         pick : "",
+         letWinner : false,
      }
  }
 
@@ -24,7 +25,7 @@ var config = {
     projectId: "rpsgame-31571",
     storageBucket: "rpsgame-31571.appspot.com",
     messagingSenderId: "983422850754"
-  };
+  }; 
   firebase.initializeApp(config);
 // create a reference to firebase
   var database = firebase.database();
@@ -33,18 +34,27 @@ var config = {
 
   var connectedRef = database.ref(".info/connected");
   
+  //when the user is connected add them to the connections list
+  connectedRef.on("value", function(snap){
   if (snap.val()) {
       var con = connectionsRef.push(true);
       //remove user from connections when they disconnect
       con.onDisconnect().remove();
   }; 
-  $("#submitBtn").on("click", function(){
+}); 
+  
+$("#submitBtn").on("click", function(event){
+    event.preventDefault();
+    name = $("#name-input").val().trim(); 
+    console.log(name); 
+    $("#playerOne").text(name); 
 
-  })
-  //updates page when playerOne info has changed
-  database.ref("/playerOne").on("value", function(snapshot){
-      if (snapshot.child("name").exists()) {
-        name = snapshot.val().name;
-      }
-  })
+//   database.ref("/RPSgame").on("value", function(snapshot){
+//       if (snapshot.child("playerOne").exists() && snapshot.child("pick").exists()) {
+//         name = snapshot.val().name;
+//         pick = snapshot.val().pick; 
+//         //change the HTML to reflect local values
+//         $("#playerOne").text(name);
+      
 
+}); 
