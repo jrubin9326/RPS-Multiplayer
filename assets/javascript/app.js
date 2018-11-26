b744438409d72ce33ce4b6fd3200ede434da2bae
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-26 16:15:49 -0500

    firestore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,28 @@
 // set inital conditions of object to start game
 
- var readyGame = {
-     playerOne : {
-        wins: 0,
-        losses: 0,
-        name : "",
-        pick : "",
-     },
-     playerTwo : {
-         wins: 0,
-         losses: 0,
-         name : "",
-         pick : "",
-         letWinner : false,
-     }
- }
+//  var readyGame = {
+//      playerOne : {
+//         wins: 0,
+//         losses: 0,
+//         name : "",
+//         pick : "",
+//         player: ""
+//      },
+//      playerTwo : {
+//          wins: 0,
+//          losses: 0,
+//          name : "",
+//          pick : "",
+//          player :""
+//      }
+//  }
+
 
 //global variables
 var playerOneChoice ;
 var playerOneWin;
 var playerTwoWin
+var playersInGame; 
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyBUzClX_NxCqGXDswwFfk7GuHG719hbxPo",
@@ -28,11 +31,36 @@ var config = {
     projectId: "rpsgame-31571",
     storageBucket: "rpsgame-31571.appspot.com",
     messagingSenderId: "983422850754"
-  }; 
+  };
   firebase.initializeApp(config);
-// create a reference to firebase
-  var database = firebase.database();
+// create a reference to firestore
+  const db = firebase.firestore();
 
+  db.settings({timestampsInSnapshots: true });
+
+  db.collection("playerOne").get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+          console.log(doc.data()); 
+          playerOneName.text(doc.data().name); 
+          
+      }) 
+  })
+
+  db.collection("playerTwo").get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+          console.log(doc.data().name)
+          playerTwoName.text(doc.data().name)
+      })
+  }); 
+ const playerOneName = $("#playerOne");
+const playerTwoName = $("#playerTwo"); 
+
+
+ function renderPlayer (doc) {
+     playerOneName.text(doc.data().name); 
+
+ }
+ renderPlayer()
   var connectionsRef = database.ref("/connections")
 
   var connectedRef = database.ref(".info/connected");
@@ -45,35 +73,52 @@ var config = {
       con.onDisconnect().remove();
   }; 
 }); 
+
 //  user can select rock paper or scissors 
-$(".choices").on("click", ".choice", function(){
-   playerOneChoice = ($(this).text())
-   console.log(playerOneChoice)
+// $(".choices").on("click", ".choice", function(){
+//    playerOneChoice = ($(this).text())
+//    console.log(playerOneChoice)
    
-})
+// })
 
-
+var playerOne;
+var playerTwo; 
 $("#submitBtn").on("click", function(event){
     event.preventDefault();
-    readyGame.playerOne.name = $("#name-input").val().trim(); 
-    console.log(name); 
-   database.ref().push({
-       playerOneName:readyGame.playerOne.name,
-       playerOneChoice:playerOneChoice
-   })
+    playerOne = $("#name-input").val().trim(); 
+    console.log(playerOne); 
+    $("#playerOne").text(playerOne); 
+//    database.ref().set({
+//        playerOne
+//    })
 }); 
 
+// database.ref("/playerOne").on("value", function (snapshot) {
+    if (playerOne !== "") {
+        playerTwo = $("#name-input").val().trim()
+        console.log(playerTwo)
+        $("#playerTwo").text(playerTwo);
+    // } database.ref().set({
+    //     playerTwo:playerTwo
+    }
+// })
+console.log(playerOne, playerTwo)
 
-database.ref().on("value", function(snapshot){
-    if (Object.keys(snapshot.val().indexOf() > -1)) {
-    playerOne = (childSnapshot.val().name)
-    $("#playerOne").text(playerOne)
-// function to handle errors
-}, function(errorObject) {
-    console.log("Errors handled: "+ errorObject.code)
-});
 
-//logic behind game 
+// database.ref("/playerOne").on("value", function(snapshot){
+//     if (Object.keys(snapshot.val().indexOf() === -1)) {
+//     playerOne =(childSnapshot.val())
+//     playersInGame = 1
+    
+// } else if (playersInGame === 1){
+//        playerTwo = (childSnapshot.val()); 
+
+// // function to handle errors
+// }).catch( function(errorObject) {
+//     console.log("Errors handled: "+ errorObject.code)
+// }); 
+
+// logic behind game 
 
 if (playerOneChoice === 'rock' && playerTwoChoice === 'paper') {
     playerOneWin++
@@ -91,4 +136,5 @@ if (playerOneChoice === 'rock' && playerTwoChoice === 'paper') {
     console.og(tie)
 } else if (playerOneChoice === 'rock' && playerTwoChoice === 'scissor') {
     playerOneWin ++
-}
+
+}; 
